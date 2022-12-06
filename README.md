71c27853da4f38af2c0a35df5b56cb0535fbde36
anushkamittal20 <anumittal4641@gmail.com>
2022-12-06 22:06:34 +0530

    iterations
    Signed-off-by: anushkamittal20 <anumittal4641@gmail.com>

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 Covers all programs and description for each program for parallel programming lab internals
 
 Mergesort execution
+```
 gcc -fopenmp -o obj mergesort.c
 ./obj
-
+```
 pi
+```
 gcc -fopenmp -o obj pi.c -lm
 export OMP_NUM_THREADS=1 
 ./obj
+```
+iterations
+```
+gcc -fopenmp -o obj iter.c
+export OMP_NUM_THREADS=2
+./obj
+```
---
+++ iteration/iter.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include<omp.h>
+
+int main(int argc,char *argv[]){
+    int iterns,i,itern=1;
+    iterns=8;
+    #pragma omp parallel for schedule(static,2)
+    for(i=1; i<=iterns; i++){
+        int t = omp_get_thread_num();
+        itern+=1;
+        itern%=2;
+        printf("thread %d iter no %d value: %d\n",t,itern+1,i);
+    }
+}
